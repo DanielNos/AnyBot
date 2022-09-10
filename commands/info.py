31a434321dee9087a596106b74650a3cd4b3eaeb
Daniel Nos <nos.daniel@pm.me>
2022-09-10 13:23:10 +0100

    Add Bot Info

--- commands/info.py
+++ commands/info.py
@@ -5,28 +5,26 @@ from nextcord import slash_command, Embed, Interaction, Member
 sys.path.append("../NosBot")
 import logger as log
 import dataManager
-from formatting import long_datetime
+from formatting import long_datetime, complete_name
 
 TEST_GUILDS = dataManager.load_test_guilds()
 PRODUCTION = dataManager.is_production()
-logger = None
 
 
 class Info(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
-        global logger
-        logger = log.Logger("./logs/log.txt")
+        self.logger = log.Logger("./logs/log.txt")
     
 
     @slash_command(guild_ids=TEST_GUILDS, description="Show information about a user. If no target is specified target will be author.", force_global=PRODUCTION)
     async def user_info(self, interaction: Interaction, user: Member = None):
         # Log
-        log_message = interaction.user.name + "#" + interaction.user.discriminator + " has called command: user_info"
+        log_message = complete_name(interaction.user) + " has called command: user_info."
         
         if user != None:
             log_message += " " + user.name
-        logger.log_info(log_message + ".")
+        self.logger.log_info(log_message + ".")
 
         # Get user
         target: Member = user or await interaction.guild.fetch_member(interaction.user.id)
@@ -58,14 +56,28 @@ class Info(commands.Cog):
     @slash_command(guild_ids=TEST_GUILDS, description="Says when a member joined.", force_global=PRODUCTION)
     async def joined(self, interaction: Interaction, member: nextcord.Member = None):
         # Log
-        log_message = interaction.user.name + "#" + interaction.user.discriminator + " has called command: joined"
+        log_message = complete_name(interaction.user)  + " has called command: joined."
 
         if member != None:
             log_message += " " + member.name
-        logger.log_info(log_message + ".")
+        self.logger.log_info(log_message + ".")
 
         user: Member = member or interaction.user
         await interaction.response.send_message(user.name + " joined " + long_datetime(user.joined_at) + ".", ephemeral=True)
+    
+
+    @slash_command(guild_ids=TEST_GUILDS, description="Show info about me.", force_global=PRODUCTION)
+    async def nosbot(self, interaction: Interaction):
+        self.logger.log_info(complete_name(interaction.user)  + " has called command: nosbot.")
+
+        # Create embed
+        embed: Embed = Embed(title="NosBot", description="A multipurpose Discord bot.", color=0xFBCE9D)
+        embed.set_thumbnail("https://raw.githubusercontent.com/4lt3rnative/nosbot/main/nosbot.png")
+
+        embed.add_field(name="You can use me to:", value="🎮 **Play games\n👮‍♂️ Moderate your guild\n📊 Create polls\n🏷️ Create role givers\n📝 Create embeds\n🎛️ Use sound board\n... And more!**", inline=False)
+        embed.add_field(name="Use /commands for list of all commands. Use /help to show the actions of commands.", value="Thank you for using me!", inline=False)
+
+        await interaction.response.send_message(embed=embed)
     
 
 def load(client: commands.Bot):
