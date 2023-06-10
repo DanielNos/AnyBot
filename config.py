62baa0ae88c0b2c7d4b3dfa5d1bf2645eeb60b13
Daniel Nos <nos.daniel@pm.me>
2023-06-10 19:54:43 +0200

    Rewrote main.py
    Started rewriting to discord.py and newer Discord API version.

---
+++ config.py
@@ -0,0 +1,59 @@
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "\033[0;97m%(asctime)s   \033[1;96m%(levelname)-6s \033[93m%(asctime)s   \033[1;97m%(module)-10s   \033[0;97m%(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+        "standard": {
+            "format": "\033[0;97m%(asctime)s   \033[1;96m%(levelname)-6s \033[93m%(name)-15s\033[0;97m   %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+        "chat": {
+            "format": "\033[0;97m%(asctime)s   \033[1;92mCHAT   \033[93m%(name)-15s\033[0;97m   %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard"
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/nosbot.log",
+            "formatter": "verbose"
+        },
+        "chatConsole": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "chat"
+        },
+        "chatFile": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/chat.log",
+            "formatter": "chat"
+        },
+    },
+    "loggers": {
+        "bot": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "discord": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "chat": {
+            "handlers": ["chatConsole", "chatFile"],
+            "level": "INFO",
+            "propagate": False
+        },
+    }
+}
--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-import logger as log
-import nextcord, dataManager
-from nextcord.ext import commands
-import dataManager
+import os, discord, asyncio, logging
+from discord.ext import commands
+from logging.config import dictConfig
+
+import config
 
 
-# Setup logger
-logger = log.Logger("./logs/log.txt")
+async def load_cogs(client: commands.Bot):
+    logger: logging.Logger = logging.getLogger("bot")
+    logger.info("Loading cogs.")
 
-# Global variables
-CONFIG = dataManager.load_config()
-logger.log_info("Config loaded.")
+    for file in os.listdir("./cogs"):
+        if not file.endswith(".py"):
+            continue
 
-# Set intents
-intents = nextcord.Intents.default()
-intents.guild_messages = True
-intents.message_content = True
-intents.guild_reactions = True
-intents.members = True
-
-client = commands.Bot(command_prefix="?", intents=intents)
-
-# Load commands
-logger.log_info("Loading commands...")
-
-dataManager.import_commands("./commands/", client)
-
-logger.log_info("Commands loaded.")
+        await client.load_extension("cogs." + file[:-3])
+        logger.info(f"Loaded cog: {file[:-3]}")
 
 
-# Run bot
-client.run(CONFIG["token"])
+
+if __name__ == "__main__":
+    # Collect token
+    token = os.getenv("NOSBOT_DISCORD_API_TOKEN")
+    if token == None:
+        print("No Discord API token set. Assign your API token to the NOSBOT_DISCORD_API_TOKEN enviroment variable.")
+        exit(1)
+
+    # Setup logger
+    if not os.path.isdir("./logs"):
+        os.mkdir("./logs")
+    dictConfig(config.LOGGING_CONFIG)
+
+    # Start client
+    client: commands.Bot = commands.Bot(command_prefix="NOPE", intents=discord.Intents.all())
+
+    asyncio.run(load_cogs(client))
+
+    client.run("OTkwMjc2MzEzMjg3ODg4ODk2.GOG5Hv.oSS-XAI76g6In4Qp5R0LSms7zqqpqLRPZ5ojLo", root_logger=True)
