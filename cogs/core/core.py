423762672e85eaeb0c965defebde886e5b928804
Daniel Nos <nos.daniel@pm.me>
2023-06-23 20:00:54 +0200

    Updated Logging of Messages
    Added logging of attachments.

--- cogs/core/core.py
+++ cogs/core/core.py
@@ -19,9 +19,29 @@ class Core(Cog):
 
     @Cog.listener()
     async def on_message(self, message: Message):
-        # Log messages from users
-        if not message.author.bot:
-            self.chat.info(f"[{message.author.name}] {message.content}")
+        if message.author.id == self.client.user.id:
+            return
+
+        text = ""
+
+        # Log user type
+        if message.author.bot:
+            text += "[bot]"
+
+        text += f"[{message.author.name}]"
+
+        # Log attachments
+        if len(message.attachments) > 0:
+            text += f" [attachments:"
+            
+            for attachment in message.attachments:
+                text += f" ({attachment.filename} : {attachment.url}"
+
+            text += "]"
+
+        text += f" {message.content}"
+
+        self.chat.info(text)
 
 
     @slash_command(name="nosbot", description=f"Shows information about {config.BOT['name']}.", guild_ids=config.DEBUG["test_guilds"])
